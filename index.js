fcec58ec6a07c5035400d579a86ad1cf453774fe
Blandon Tan <e170095@e.ntu.edu.sg>
2020-05-30 10:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.unshift("Bob")
 }
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(name){
+  
+}
